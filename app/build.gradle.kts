8549b870580f1f4a61531b268cce6492c71dbd8c
kewashi1 <takeshi.kamakura111111@gmail.com>
2022-07-16 05:27:28 +0900

    lint 削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,6 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("org.jlleitschuh.gradle.ktlint")
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
